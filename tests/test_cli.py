3ea18ff01b688e692d80855f416e16e93e73a23a
Nemo <commits@captnemo.in>
2021-07-16 16:57:09 +0530

    [tests] Add tests for argument parser

---
+++ tests/test_cli.py
@@ -0,0 +1,15 @@
+from pystitcher.skeleton import parse_args
+import logging
+
+def test_default_args():
+    args = parse_args(['tests/book-clean.md', 'o.pdf'])
+    assert args.loglevel == None
+    assert args.cleanup == True
+
+def test_loglevel():
+    args = parse_args(['-v', 'tests/book-clean.md', 'o.pdf'])
+    assert args.loglevel == logging.INFO
+
+def test_cleanup():
+    args = parse_args(['--cleanup', 'tests/book-clean.md', 'o.pdf'])
+    assert args.cleanup == True
